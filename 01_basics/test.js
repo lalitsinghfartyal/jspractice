23477f8c70733d7fcdc0c18e83295a32d9573019
lalitsinghfartyal <lalitsinghfartyal@users.noreply.github.com>
2023-09-30 11:17:59 +0000

    Changes to be committed

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,19 @@
-console.log("lalit");
+const accountId = 101
+let accountEmail = "lalitsanchay@gmail.com"
+var accountPassword = "1234"
+accountCity = "Delhi"
+let accountState;
+
+accountEmail = "lalityogitpain@gmail.com"
+accountPassword = "abcd"
+accountCity = "UK"
+
+/*
+Prefer not to use var 
+because of issue in block scope and functionaal scope
+*/
+
+
+console.log(accountCity)
+
+console.table([accountEmail, accountPassword, accountCity, accountState])
