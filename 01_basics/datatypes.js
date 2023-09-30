16e5046312f615bce2f16bda6dcd3dea66497708
lalitsinghfartyal <lalitsinghfartyal@users.noreply.github.com>
2023-09-30 11:22:11 +0000

    add Datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,35 @@
+"use strict";  // treat all JS codde as newer version
+
+// alert( 3 + 3);   // We are using nodejs, not browser
+
+
+console.log(3 
+    + 
+    3)  //code readability should be high
+
+console.log("Lalit")
+
+
+let name = "Lalit"
+let age = 32
+let isLoggedIn = false
+
+
+// premative Datatype
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique 
+
+
+
+// objecct
+
+console.log(typeof undefined)
+
+console.log(typeof null)
+// https://tc39.es/ecma262/#sec-intro
